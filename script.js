a6afe6b619dd888df1ce8fe0780b5210937180ca
Samsul Alom Laskar <laskarikbal126@gmail.com>
2023-03-01 23:30:09 +0530

    AJAX call & callback hell

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const countriesContainer = document.querySelector(".countries");
 //? src attribute of html is asynchronous in nature - once the image gets load the JS then emit the load event & then we can listen to that event
 
 //* AJAX call
-
+/* 
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
@@ -44,3 +44,74 @@ const getCountryData = function (country) {
 
 getCountryData("india");
 getCountryData("usa");
+
+ */
+
+///////////////////////////////////////
+
+//? src attribute of html is asynchronous in nature - once the image gets load the JS then emit the load event & then we can listen to that event
+
+const renderCountry = function (data, className = "") {
+  const [currencyKey] = Object.keys(data.currencies);
+  const [languageKey] = Object.keys(data.languages);
+  console.log(languageKey);
+
+  const html = ` 
+    <article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[languageKey]}</p>
+      <p class="country__row"><span>💰</span>${
+        data.currencies[currencyKey].name
+      }</p>
+        </div>
+        </article>
+        `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+//* AJAX call for country 1
+
+const getCountryAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+
+    //! Render country 1
+    renderCountry(data);
+
+    //? get neighbour country
+    const [neighbour] = data.borders;
+    console.log(neighbour);
+
+    if (!neighbour) return;
+
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbour}`);
+
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, "neighbour");
+    });
+  });
+};
+
+getCountryAndNeighbour("india");
+// getCountryAndNeighbour("usa");
+
+// Domain name is validated by the DNS with the original IP address
+
+//? Call back hell is defined as when there's a lot of nested call back's in order to execute an asynchronous task in sequence - it makes our code hard to maintain & difficult to understand
