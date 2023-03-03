f6bdb08506a97ba4611653c6a3075d0253d555e2
Samsul Alom Laskar <laskarikbal126@gmail.com>
2023-03-03 11:25:39 +0530

    returning values from async function

--- asyncAwait.js
+++ asyncAwait.js
@@ -3,6 +3,32 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
+const renderCountry = function (data, className = "") {
+  const [currencyKey] = Object.keys(data.currencies);
+  const [languageKey] = Object.keys(data.languages);
+
+  const html = ` 
+        <article class="country ${className}">
+        <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+          <p class="country__row"><span>🗣️</span>${
+            data.languages[languageKey]
+          }</p>
+          <p class="country__row"><span>💰</span>${
+            data.currencies[currencyKey].name
+          }</p>
+            </div>
+            </article>
+            `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText("beforeend", msg);
   //
@@ -29,48 +55,45 @@ const whereAmI = async function (country) {
     if (!resGeo.ok) throw new Error("Problem in getting location data");
 
     const dataGeo = await resGeo.json();
-    console.log(dataGeo);
 
     const resp = await fetch(
       `https://restcountries.com/v3.1/name/${dataGeo.country}`
     );
     if (!resp.ok) throw new Error("Problem in getting country");
     const data = await resp.json();
-    console.log(data);
-    renderCountry("something");
-    // renderCountry(data[0]);
+    renderCountry(data[0]);
+
+    //? returning values from async function
+    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
   } catch (err) {
     renderError(err.message);
-    // console.error(err.message);
+
+    // reject promise returned from async function
+    throw err;
   }
 };
 
-console.log("first");
-whereAmI();
-console.log("second");
+console.log("Will get location");
+// const city = whereAmI();
+// console.log(city);
+/*
+ whereAmI()
+  .then((city) => console.log(city))
+  .catch((err) => console.error(err.message))
+  .finally(() => console.log("Finished getting the location"));
+   */
 
-const renderCountry = function (data, className = "") {
-  const [currencyKey] = Object.keys(data.currencies);
-  const [languageKey] = Object.keys(data.languages);
+// whereAmI()
+//   .then((city) => console.log(city))
+//   .catch((err) => console.error(err.message))
+//   .finally(() => console.log("Finished getting the location"));
 
-  const html = ` 
-      <article class="country ${className}">
-      <img class="country__img" src="${data.flags.png}" />
-      <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} people</p>
-        <p class="country__row"><span>🗣️</span>${
-          data.languages[languageKey]
-        }</p>
-        <p class="country__row"><span>💰</span>${
-          data.currencies[currencyKey].name
-        }</p>
-          </div>
-          </article>
-          `;
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
-};
+(async function () {
+  try {
+    const city = await whereAmI();
+    console.log(city);
+  } catch (err) {
+    console.error(err.message);
+  }
+  console.log("Finished getting the location");
+})();
